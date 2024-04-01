256d875ee15c79c07fd5bddb0e6f3da656522c0c
Mikhail Bobrenko <megabeaver2024@gmail.com>
2024-04-01 16:32:25 +0600

    feat: добавить компоненты хидер и футер

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import Header from "./header"
+import Footer from "./Footer"
+
+function App() {
+  return (
+    <div className="page">
+      <Header />
+
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import logoHeader from '../images/logos/NFL_logo-min.svg'
+
+function Header() {
+    return (
+        <header className="header">
+            <img className="header__logo" src={logoHeader} alt="Логотип Место" />
+            <div className='header__links-container'>
+                {/* {isLoggedIn
+          ? <>
+            <p className='header__email'>{userEmail}</p>
+            <Link to='/sign-in' className='header__link' onClick={onLinkClick}>Выйти</Link>
+          </>
+          : <Link to={isRegistration ? '/sign-in' : '/sign-up'} className='header__link' onClick={onLinkClick}>
+            {isRegistration ? 'Войти' : 'Регистрация'}
+          </Link>
+        } */}
+            </div>
+        </header>
+    );
+}
+
+export default Header
